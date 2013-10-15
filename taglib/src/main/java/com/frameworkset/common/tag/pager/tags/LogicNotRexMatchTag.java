5b7021e8198f3692e587458c5646bb549e83945f
gao.tang@oceanwing.com <gao.tang@oceanwing.com>
2013-10-15 22:57:00 +0800

    add

---
+++ taglib/src/main/java/com/frameworkset/common/tag/pager/tags/LogicNotRexMatchTag.java
@@ -0,0 +1,17 @@
+﻿package com.frameworkset.common.tag.pager.tags;
+
+import com.frameworkset.util.RegexUtil;
+
+public class LogicNotRexMatchTag extends MatchTag {
+	
+	protected boolean match() {
+		
+		if(actualValue == null)
+			return false;
+		if(!RegexUtil.isMatch(String.valueOf(actualValue),pattern))
+			return true;
+		else
+			return false;
+	}
+
+}
